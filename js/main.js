518ccf3b8d120b4dc9f5cfbff8e443dbb6a06b40
sasiok <sashokivanovyck@gmail.com>
2024-11-29 23:38:30 +0200

    v1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // ========================================================= Меню-бургер =================================
 let bodyLockStatus = true;
+
 let bodyLock = (delay = 500) => {
     let body = document.querySelector("body");
     if (bodyLockStatus) {
@@ -53,18 +54,31 @@ function menuInit() {
                 document.documentElement.classList.toggle("menu-open");
             }
         });
-    };
+
+        // Додаємо обробник для пунктів меню
+        const menuLinks = document.querySelectorAll(".menu__link");
+        if (menuLinks.length) {
+            menuLinks.forEach(link => {
+                link.addEventListener("click", () => {
+                    menuClose(); // Закриваємо меню при натисканні на пункт
+                });
+            });
+        }
+    }
 }
+
 function menuOpen() {
     bodyLock();
     document.documentElement.classList.add("menu-open");
 }
+
 function menuClose() {
     bodyUnlock();
     document.documentElement.classList.remove("menu-open");
 }
 
 menuInit();
+
 
 // ==================================================================== Dinamic Adaptive =======================================================
 
@@ -247,3 +261,165 @@ setTimeout(() => {
         });
     }
 }, 0);
+
+// ==================================================================== Smooth scroll =======================================================
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 500,
+    header: ".header"
+});
+
+// ==================================================================== Watcher =======================================================
+// Спостерігач об'єктів [всевидюче око]
+// data-watch - можна писати значення для застосування кастомного коду
+// data-watch-root - батьківський елемент всередині якого спостерігати за об'єктом
+// data-watch-margin -відступ
+// data-watch-threshold - відсоток показу об'єкта для спрацьовування
+// data-watch-once - спостерігати лише один раз
+// _watcher-view - клас який додається за появи об'єкта
+function uniqArray(array) {
+    return array.filter(function (item, index, self) {
+        return self.indexOf(item) === index;
+    });
+}
+
+class ScrollWatcher {
+    constructor(props) {
+        let defaultConfig = {
+            logging: true,
+        };
+        this.config = Object.assign(defaultConfig, props);
+        this.observer;
+
+        // Перевірка, чи є вже клас 'watcher' на кореневому елементі
+        if (!document.documentElement.classList.contains('watcher')) {
+            this.scrollWatcherRun();
+        }
+    }
+
+    // Оновлення конструктора
+    scrollWatcherUpdate() {
+        this.scrollWatcherRun();
+    }
+
+    // Запуск конструктора
+    scrollWatcherRun() {
+        document.documentElement.classList.add('watcher');
+        this.scrollWatcherConstructor(document.querySelectorAll('[data-watch]'));
+    }
+
+    // Конструктор спостерігачів
+    scrollWatcherConstructor(items) {
+        if (items.length) {
+            this.scrollWatcherLogging(`Прокинувся, стежу за об'єктами (${items.length})...`);
+            let uniqParams = uniqArray(Array.from(items).map(item => `${item.dataset.watchRoot ? item.dataset.watchRoot : null}|${item.dataset.watchMargin ? item.dataset.watchMargin : '0px'}|${item.dataset.watchThreshold ? item.dataset.watchThreshold : 0}`));
+
+            uniqParams.forEach(uniqParam => {
+                let uniqParamArray = uniqParam.split('|');
+                let paramsWatch = {
+                    root: uniqParamArray[0],
+                    margin: uniqParamArray[1],
+                    threshold: uniqParamArray[2]
+                };
+
+                let groupItems = Array.from(items).filter(item => {
+                    let watchRoot = item.dataset.watchRoot ? item.dataset.watchRoot : null;
+                    let watchMargin = item.dataset.watchMargin ? item.dataset.watchMargin : '0px';
+                    let watchThreshold = item.dataset.watchThreshold ? item.dataset.watchThreshold : 0;
+                    return (
+                        String(watchRoot) === paramsWatch.root &&
+                        String(watchMargin) === paramsWatch.margin &&
+                        String(watchThreshold) === paramsWatch.threshold
+                    );
+                });
+
+                let configWatcher = this.getScrollWatcherConfig(paramsWatch);
+                this.scrollWatcherInit(groupItems, configWatcher);
+            });
+        } else {
+            this.scrollWatcherLogging("Сплю, немає об'єктів для стеження. ZzzZZzz");
+        }
+    }
+
+    // Функція створення налаштувань
+    getScrollWatcherConfig(paramsWatch) {
+        let configWatcher = {};
+        if (document.querySelector(paramsWatch.root)) {
+            configWatcher.root = document.querySelector(paramsWatch.root);
+        } else if (paramsWatch.root !== 'null') {
+            this.scrollWatcherLogging(`Емм... батьківського об'єкта ${paramsWatch.root} немає на сторінці`);
+        }
+
+        configWatcher.rootMargin = paramsWatch.margin;
+        if (paramsWatch.margin.indexOf('px') < 0 && paramsWatch.margin.indexOf('%') < 0) {
+            this.scrollWatcherLogging(`Йой, налаштування data-watch-margin потрібно задавати в PX або %`);
+            return;
+        }
+
+        if (paramsWatch.threshold === 'prx') {
+            paramsWatch.threshold = [];
+            for (let i = 0; i <= 1.0; i += 0.005) {
+                paramsWatch.threshold.push(i);
+            }
+        } else {
+            paramsWatch.threshold = paramsWatch.threshold.split(',');
+        }
+        configWatcher.threshold = paramsWatch.threshold;
+
+        return configWatcher;
+    }
+
+    // Функція створення нового спостерігача зі своїми налаштуваннями
+    scrollWatcherCreate(configWatcher) {
+        this.observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                this.scrollWatcherCallback(entry, observer);
+            });
+        }, configWatcher);
+    }
+
+    // Функція ініціалізації спостерігача зі своїми налаштуваннями
+    scrollWatcherInit(items, configWatcher) {
+        this.scrollWatcherCreate(configWatcher);
+        items.forEach(item => this.observer.observe(item));
+    }
+
+    // Функція обробки базових дій точок спрацьовування
+    scrollWatcherIntersecting(entry, targetElement) {
+        if (entry.isIntersecting) {
+            !targetElement.classList.contains('_watcher-view') ? targetElement.classList.add('_watcher-view') : null;
+            this.scrollWatcherLogging(`Я бачу ${targetElement.classList}, додав клас _watcher-view`);
+        } else {
+            targetElement.classList.contains('_watcher-view') ? targetElement.classList.remove('_watcher-view') : null;
+            this.scrollWatcherLogging(`Я не бачу ${targetElement.classList}, прибрав клас _watcher-view`);
+        }
+    }
+
+    // Функція вимкнення стеження за об'єктом
+    scrollWatcherOff(targetElement, observer) {
+        observer.unobserve(targetElement);
+        this.scrollWatcherLogging(`Я перестав стежити за ${targetElement.classList}`);
+    }
+
+    // Функція виведення в консоль
+    scrollWatcherLogging(message) {
+        this.config.logging ? console.log(`[Спостерігач]: ${message}`) : null;
+    }
+
+    // Функція обробки спостереження
+    scrollWatcherCallback(entry, observer) {
+        const targetElement = entry.target;
+        this.scrollWatcherIntersecting(entry, targetElement);
+
+        if (targetElement.hasAttribute('data-watch-once') && entry.isIntersecting) {
+            this.scrollWatcherOff(targetElement, observer);
+        }
+
+        document.dispatchEvent(new CustomEvent("watcherCallback", {
+            detail: {
+                entry: entry
+            }
+        }));
+    }
+}
+
+new ScrollWatcher({});
